5d7237a2d44f69a03aa90be96dda268790fcca64
Ilya <isidash@itex.ru>
2017-12-13 17:21:39 +0500

    animate was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,18 @@
-var animated = (function(){
+var featuresItems = $('.features__item');
+var marginTop = $(window).height() / 1.5;
 
-  return {
-    show: function(elem, class) {
-      elem.classList.add(class);
-    }
+window.onscroll= function(){
+  var wScroll = $(document).scrollTop();
+  if ($('.features__item').offset().top - wScroll < marginTop  ){
+    $('.features__item').eq(0).addClass('animated fadeInLeftBig');
+    $('.features__item').eq(1).addClass('animated fadeInRightBig');
+
   }
-})();
+  if ($('.protect__content').offset().top - wScroll < marginTop  ){
+    $('.protect__content').addClass('animated fadeInRightBig');
+  }
+  if ($('.title.map-feature__title').offset().top - wScroll < marginTop  ){
+    $('.title.map-feature__title').parent().addClass('animated fadeInLeftBig');
+  }
 
-window.onscroll
+};
